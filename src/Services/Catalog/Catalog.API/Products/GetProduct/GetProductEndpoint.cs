fc38cdad21196fc76bbbeb48b9a4a2c702b53aa5
Soaad Sayed <ssayed.c@NEC.GOV.SA>
2024-07-18 17:37:38 +0300

    Adding GetProduct Endpoint and its handler

---
+++ src/Services/Catalog/Catalog.API/Products/GetProduct/GetProductEndpoint.cs
@@ -0,0 +1,23 @@
+﻿
+using Catalog.API.Products.CreateProduct;
+
+namespace Catalog.API.Products.GetProduct
+{
+    public record GetProductResponse(IEnumerable<Product>Products);
+    public class GetProductEndpointL : ICarterModule
+    {
+        public void AddRoutes(IEndpointRouteBuilder app)
+        {
+            app.MapGet("/products", async (ISender sender) => {
+                var result = await sender.Send(new GetProductQuery());
+                var response = result.Adapt<GetProductResponse>();
+
+                return Results.Ok(response);
+            }).WithName("GetProducts")
+            .Produces<GetProductResponse>(StatusCodes.Status200OK)
+            .ProducesProblem(StatusCodes.Status400BadRequest)
+            .WithDescription("Get Products")
+            .WithSummary("Get Products");
+        }
+    }
+}
---
+++ src/Services/Catalog/Catalog.API/Products/GetProduct/GetProductHandler.cs
@@ -0,0 +1,18 @@
+﻿
+using Marten.Linq.QueryHandlers;
+namespace Catalog.API.Products.GetProduct
+{
+    public record GetProductQuery():IQuery<GetProductResult>;
+    public record GetProductResult(IEnumerable<Product>Products);
+    internal class GetProductQueryHandler  (IDocumentSession session,ILogger<GetProductQueryHandler> logger)
+        : IQueryHandler<GetProductQuery, GetProductResult>
+    {
+       public async Task<GetProductResult> Handle(GetProductQuery query, CancellationToken cancellationToken)
+        {
+            logger.LogInformation("GetProductQueryHandler.Handle called with {@query}", query);
+
+            var products= await session.Query<Product>().ToListAsync(cancellationToken);   
+            return new GetProductResult(products);  
+        }
+    }
+}
