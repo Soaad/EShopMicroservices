beba9e13739180feefc714177e40d8a194905569
Soaad Sayed <ssayed.c@NEC.GOV.SA>
2024-09-25 11:38:29 +0300

    addin fluentapi to building blocks and its depencency injection

--- src/Services/Catalog/Catalog.API/Program.cs
+++ src/Services/Catalog/Catalog.API/Program.cs
@@ -1,3 +1,7 @@
+
+
+using Catalog.API.Products.CreateProduct;
+
 var builder = WebApplication.CreateBuilder(args);
 
 //add services to the container
@@ -9,6 +13,14 @@ builder.Services.AddMediatR(config =>
     config.RegisterServicesFromAssemblies(typeof(Program).Assembly);
 });
 
+builder.Services.AddValidatorsFromAssembly(typeof(Program).Assembly);
+//builder.Services.AddValidatorsFromAssemblyContaining<CreateProductCommandValidator>();
+var serviceProvider = builder.Services.BuildServiceProvider();
+var validator = serviceProvider.GetService<IValidator<CreateProductCommand>>();
+if (validator == null)
+{
+    throw new Exception("CreateProductCommandValidator is not registered.");
+}
 builder.Services.AddMarten(opts =>
 {
     opts.Connection(builder.Configuration.GetConnectionString("Database")!);
