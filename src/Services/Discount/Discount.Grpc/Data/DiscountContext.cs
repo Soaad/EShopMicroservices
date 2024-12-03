b0f63580b2a563f7e77d1c06e21dff492f2fbe56
SAbdelaziz <ssayed.c@nec.gov.sa>
2024-12-03 10:05:07 +0200

    update coupon grpc service to use sqlite db and containerize this service

---
+++ src/Services/Discount/Discount.Grpc/Data/DiscountContext.cs
@@ -0,0 +1,21 @@
+﻿using Discount.Grpc.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Discount.Grpc.Data
+{
+    public class DiscountContext:DbContext
+    {
+      public  DbSet<Coupon> Coupons { set; get; } = default;
+        public DiscountContext(DbContextOptions<DiscountContext> options):base(options)
+        {
+                
+        }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Coupon>().HasData(
+                new Coupon { Id=1,Amount=1500,Description="Iphone x with many features", ProductName="Iphon x"},
+                  new Coupon { Id = 2, Amount = 1500, Description = "Iphone pro max released on 2020", ProductName = "Iphon 11 pro max" }
+                );
+        }
+    }
+}
---
+++ src/Services/Discount/Discount.Grpc/Services/DiscountService.cs
@@ -0,0 +1,63 @@
+﻿using Discount.Grpc.Data;
+using Discount.Grpc.Models;
+using Grpc.Core;
+using Mapster;
+using Microsoft.EntityFrameworkCore;
+
+namespace Discount.Grpc.Services
+{
+    public class DiscountService(DiscountContext discountContext,ILogger <DiscountService> logger)
+        :DiscountProtoService.DiscountProtoServiceBase
+    {
+        public override async Task<CouponModel> GetDiscount(GetDiscountRequest request, ServerCallContext context)
+        {
+            var coupon=await discountContext.Coupons.FirstOrDefaultAsync(x=>x.ProductName == request.ProductName);
+            if (coupon == null) { 
+                coupon=new Coupon { ProductName = "No Discount",Amount=0,Description="No Discount" };
+            }
+
+            logger.LogInformation("Discount is retrieved for productNme : {productname} ,Amount: {Amount}",coupon.ProductName,coupon.Amount);
+            var couponModel = coupon.Adapt<CouponModel>();
+            return couponModel;
+        }
+
+        public override async Task<CouponModel> CreateDiscount(CreateDiscountRequest request, ServerCallContext context)
+        {
+            var coupon = request.Coupon.Adapt<Coupon>();
+            if (coupon == null)
+                throw new RpcException(new Status(StatusCode.InvalidArgument, "Invalid request object"));
+           discountContext.Coupons.Add(coupon);
+            await discountContext.SaveChangesAsync();
+            logger.LogInformation("Product successfully created productName : {productName}", coupon.ProductName);
+            var couponModel=coupon.Adapt<CouponModel>();    
+
+            return couponModel; 
+        }
+
+        public override async Task<DeleteDiscountResponse> DeleteDiscount(DeleteDiscountRequest request, ServerCallContext context)
+        {
+            var coupon = await discountContext.Coupons.FirstOrDefaultAsync(x => x.ProductName == request.ProductName);
+
+            if (coupon == null)
+                throw new RpcException(new Status(StatusCode.NotFound, $"product not found :{request.ProductName}"));
+            discountContext.Coupons.Remove(coupon);
+            await discountContext.SaveChangesAsync();
+            logger.LogInformation("Product successfully deleted productName : {productName}", coupon.ProductName);
+          
+            return new DeleteDiscountResponse { Success = true };
+        }
+
+        public override  async Task<CouponModel> UpdateDiscount(UpdateDiscountRequest request, ServerCallContext context)
+        {
+            var coupon = request.Coupon.Adapt<Coupon>();
+            if (coupon == null)
+                throw new RpcException(new Status(StatusCode.InvalidArgument, "Invalid request object"));
+            discountContext.Coupons.Update(coupon);
+            await discountContext.SaveChangesAsync();
+            logger.LogInformation("Product successfully updated productName : {productName}", coupon.ProductName);
+            var couponModel = coupon.Adapt<CouponModel>();
+
+            return couponModel;
+        }
+    }
+}
